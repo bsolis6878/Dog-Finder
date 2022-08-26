c15dd7f9ef47e64e76a8610954de9f7b27de1a85
bsolis6878 <bsolis6878@gmail.com>
2022-08-26 18:41:25 -0500

    random dog button brings up 3 dog images with breed name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var breedDropdown = document.querySelector("#breed-search-bar");
 var randomButton = document.querySelector("#random-button");
+var dogBox = document.querySelector("#dog-box");
 
 // adds dropdown option for every breed
 var dogSelections = function() {
@@ -20,7 +21,26 @@ var dogSelections = function() {
 var randomDogs = function() {
     var apiUrl = "https://dog.ceo/api/breeds/image/random/3";
 
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            dogBox.innerHTML = "";
+            for (i = 0; i < Object.keys(data.message).length; i++) {
+                // creates img elements
+                var dogPicture = document.createElement("img");
+                dogPicture.setAttribute("src", data.message[i]);
+                dogPicture.className = "images";
 
+                // adds breed for each image
+                var breedName = document.createElement("span");
+                breedName.textContent = data.message[i].split("/")[4];
+                dogBox.appendChild(breedName);
+
+                dogBox.appendChild(dogPicture);
+                console.log(data.message[i]);
+            }
+        })
+    })
 }
 
 dogSelections();
+randomButton.addEventListener("click", randomDogs);
